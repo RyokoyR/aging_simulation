b1cc142b9521b3dafb1f362a87688b2dc0c60bdb
RyokoyR <57166788+RyokoyR@users.noreply.github.com>
2019-10-31 11:29:54 +0900

    Create sampling.py
    script for VAE and CVAE to sample latent variables.

---
+++ スクリプト/sampling.py
@@ -0,0 +1,20 @@
+from keras import backend as K
+
+epsilon_std = 1.0
+
+
+def sampling(args):
+
+    global z
+    import tensorflow as tf
+    # Function with args required for Keras Lambda function
+    z_mean, z_log_var = args
+
+    # Draw epsilon of the same shape from a standard normal distribution
+    epsilon = K.random_normal(shape=tf.shape(z_mean), mean=0.,
+                              stddev=epsilon_std)
+
+    # The latent vector is non-deterministic and differentiable
+    # in respect to z_mean and z_log_var
+    z = z_mean + K.exp(z_log_var / 2) * epsilon
+    return z
