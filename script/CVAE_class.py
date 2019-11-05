d0e02fe4f700b7c76088c5a718b78e62f223a481
RyokoyR <mothermarktask@gmail.com>
2019-11-05 12:16:36 +0900

    行の削除

--- script/CVAE_class.py
+++ script/CVAE_class.py
@@ -201,5 +201,3 @@ class Tybalt():
         def save_models(self, encoder_file_path, decoder_file_path):
                 self.encoder.save(encoder_file_path)
                 self.decoder.save(decoder_file_path)
-
-        
